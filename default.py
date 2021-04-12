3efdb7140da1b8bdd88ca3917f0e1236fbe3afa8
chamchenko <chamchenko@gmail.com>
2021-04-12 18:09:12 +0100

    Update default.py
    change plugin id to cbsnetwork, and plugin name to CBS Network

--- default.py
+++ default.py
@@ -1,20 +1,20 @@
 #   Copyright (C) 2019 Lunatixz
 #
 #
-# This file is part of CBS.
+# This file is part of CBS Network.
 #
-# CBS is free software: you can redistribute it and/or modify
+# CBS Network is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# CBS is distributed in the hope that it will be useful,
+# CBS Network is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with CBS.  If not, see <http://www.gnu.org/licenses/>.
+# along with CBS Network.  If not, see <http://www.gnu.org/licenses/>.
 
 # -*- coding: utf-8 -*-
 
