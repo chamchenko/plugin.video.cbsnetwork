6f18e8b1059fb47d716b2292f77fdcd1345916c5
guiworldtv2 <95205982+guiworldtv2@users.noreply.github.com>
2023-02-14 17:57:08 -0300

    Update livetv.py

--- resources/lib/livetv.py
+++ resources/lib/livetv.py
@@ -25,20 +25,74 @@ PARAMS_LIVE_TV = {
 headers = {"User-Agent": USER_AGENT}
 
 def browseLiveTV():
+    # CBS
+    live_tv_url = "https://v-ny.mybtv.net/live/545/1280x720.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS"
+        addLink(title, live_url, 8)
+
+
+    # CBS
+    live_tv_url = "https://cbsn-2.cbsnstream.cbsnews.com/out/v1/a6a897e8f4f74cfc896223dfd822482f/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS Switcher 1"
+        addLink(title, live_url, 8)
+
+    # CBS
+    live_tv_url = "https://cbsn-3-fastly.cbsnstream.cbsnews.com/out/v1/f081461c65494eeaa9ec596d0fe96a14/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS Switcher 2"
+        addLink(title, live_url, 8)
+
+
+
+ # CBS News
     live_tv_url = "https://dai.google.com/linear/hls/pa/event/Sid4xiTQTkCT1SLu6rjUSQ/stream/82d2999b-0c66-4c7d-83e8-1201d7d31bbe:CBF2/variant/ea524505d39e73f6a2da30bf0adce261/bandwidth/3073312.m3u8"
     response = requests.get(live_tv_url)
-    if response.status_code != 200:
-        print("Error fetching url: " + live_tv_url)
-        return
-    live_url = response.url
-    title = "CBS News"
-    addLink(title, live_url, 8)
-    
-    
-   
-        
-        
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS News"
+        addLink(title, live_url, 8)
+
+    # CBS News 2
+    live_tv_url = "https://dai.google.com/ssai/event/Sid4xiTQTkCT1SLu6rjUSQ/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS News 2"
+        addLink(title, live_url, 8)
+
+    # CBS News Miami
+    live_tv_url = "https://dai.google.com/linear/hls/event/_yODrBHESGSzPe3dqW4gGg/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS News Miami"
+        addLink(title, live_url, 8)
+
+    # CBS News Los Angeles
+    live_tv_url = "https://dai.google.com/linear/hls/event/K_2aA7OVRTacJJli1oSi8w/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "CBS News Los Angeles"
+        addLink(title, live_url, 8)
+
+    # ET AMERICANO
+    live_tv_url = "https://dai.google.com/linear/hls/event/xrVrJYTmTfitfXBQfeZByQ/master.m3u8"
+    response = requests.get(live_tv_url)
+    if response.status_code == 200:
+        live_url = response.url
+        title = "ET AMERICANO"
+        addLink(title, live_url, 8)
+
+
+
+
 import requests
-
-
-        
